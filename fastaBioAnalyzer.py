436ff4cf3aaa639f8b997386aa44ddc1ff937cad
Zee Bunny <aar198184@oconeeschools.org>
2025-06-30 17:17:40 -0400

    Basic Biopython code and Tools for analyzing FASTA and Genbank Data Files

---
+++ fastaBioAnalyzer.py
@@ -0,0 +1,7 @@
+from Bio import SeqIO
+
+for seq_record in SeqIO.parse("C:/Users/zeebu/OneDrive/Documents/Python/Basic-Bioinformatics-Tools/ls_orchid.fasta", "fasta"):
+    print(seq_record.id)
+    print(repr(seq_record.seq))
+    print(len(seq_record))
+
